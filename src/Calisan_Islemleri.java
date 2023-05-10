2136984ba926417e449ba52894ef668d8c60df70
gulhante <gulhante@gmail.com>
2023-05-10 14:31:00 +0200

    proje eklendi

---
+++ src/Calisan_Islemleri.java
@@ -0,0 +1,16 @@
+
+public interface Calisan_Islemleri {
+
+    void calisanlar();
+    void urunTanimlama();
+    void miktarGuncelleme();
+    void rafGuncelleme();
+    void urunSilme();
+    void urunListeleme();
+    void urunArama();
+    void indirimYapma();
+    void giris();
+
+
+}
+
